11bc0e59589480cb6713f5ac2ee3329585302f2c
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-12-25 11:42:06 -0500

    test

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,11 +21,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
-
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
