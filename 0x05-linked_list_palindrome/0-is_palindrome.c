6e573eb5d2add4fba534af2e8cef5f26a686f51b
root <root@LAPTOP-4MQ15EMS.localdomain>
2022-01-20 20:05:45 -0500

    .

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Head of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+int list[1000000]; /* Buffer for the list */
+long n = 0, /* Number of elements */ i; /* Iterator of elements */
+listint_t *node;
+
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	if ((*head)->next == NULL)
+		return (1);
+
+	node = *head;
+	while (node != NULL)
+	{
+		list[n] = node->n;
+		node = node->next;
+		n++;
+	}
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		if (list[i] != list[n - i - 1])
+			return (0);
+	}
+
+	return (1);
+}
