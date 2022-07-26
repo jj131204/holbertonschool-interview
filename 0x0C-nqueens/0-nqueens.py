3ed4856129742a28debd8c8ca1092a39ffc89927
jj131204 <jj131204@gmail.com>
2022-07-25 22:23:15 -0500

    0x0c

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,54 +1,43 @@
 #!/usr/bin/python3
-"""Solving N Queens with Backtracing"""
 import sys
 
 
-def nqueens(n, y, board):
+def nqueens(queens, xy_diff, xy_sum):
+    """Args:
+        queens: columns occupied by queens
+        xy_diff: positive slope diagonals occupied by queens
+        xy_sum: negative slope diagonals occupied by queens
     """
-    Method: nqueens - place n queens
-            on an n by n board so that
-            no queens are attacking any
-            others.
-    Parameters: n is an int that sets
-                board size and # of queens
-    Return: All possible solutions to
-            placement, in list of lists form
-    """
-    for x in range(n):
-        hold = 0
-        for q in board:
-            if x == q[1]:
-                hold = 1
-                break
-            if y - x == q[0] - q[1]:
-                hold = 1
-                break
-            if x - q[1] == q[0] - y:
-                hold = 1
-                break
-        if hold == 0:
-            board.append([y, x])
-            if y != n - 1:
-                nqueens(n, y + 1, board)
-            else:
-                print(board)
-            del board[-1]
+    p = len(queens)
+    if p == n:
+        queen_col.append(queens)
+        return None
+    for q in range(n):
+        if q not in queens and p - q not in xy_diff and p + q not in xy_sum:
+            nqueens(queens + [q], xy_diff + [p - q], xy_sum + [p + q])
 
 
-def main():
+def parse_argv():
+    """Checks for usage errors"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print('N must be a number')
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
         sys.exit(1)
+    n = int(sys.argv[1])
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
+    return n
 
-    nqueens(n, 0, [])
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    n = parse_argv()
+    queen_col = []
+    nqueens([], [], [])
+    for row in range(len(queen_col)):
+        queen_pos = []
+        for col in range(len(queen_col[row])):
+            queen_pos.append([col, queen_col[row][col]])
+        print(queen_pos)
